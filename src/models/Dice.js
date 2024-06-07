514f4983d713480a3f1b41538a612d61f9ec948b
Harshita Makode <96257348+harshita-2002-m@users.noreply.github.com>
2024-06-08 01:47:43 +0530

    Update Dice.js

--- src/models/Dice.js
+++ src/models/Dice.js
@@ -1,1 +1,10 @@
+// Defines the Dice class, representing a six-sided dice used in the game.
 
+class Dice {
+    // This is to roll the dice, returning a value between 1 and 6
+    roll() {
+        return Math.floor(Math.random() * 6) + 1;
+    }
+}
+
+module.exports = Dice;
