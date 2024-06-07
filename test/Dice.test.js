a05d8c805a74a85e1fcb559768b4eb0fdd05340c
Harshita Makode <96257348+harshita-2002-m@users.noreply.github.com>
2024-06-08 01:58:23 +0530

    Update Dice.test.js

--- test/Dice.test.js
+++ test/Dice.test.js
@@ -1,1 +1,16 @@
+// Unit tests for the Dice class.
+
+const assert = require('assert');
+const Dice = require('../src/models/Dice');
+
+describe('Dice', function() {
+    // Test the roll method to ensure it returns a value between 1 and 6
+    it('should roll a value between 1 and 6', function() {
+        const dice = new Dice();
+        for (let i = 0; i < 100; i++) {
+            const roll = dice.roll();
+            assert(roll >= 1 && roll <= 6);
+        }
+    });
+});
 
